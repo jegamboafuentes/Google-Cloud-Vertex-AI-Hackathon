a4da38d5c21f79a6acfa63e40de357c32ce32711
GustablEnano <EliasOrdazS.1995@gmail.com>
2023-07-07 23:26:47 -0600

    Test funcionando

--- chromeextention0/background.js
+++ chromeextention0/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "translate",
-      title: "Translate to Slang",
+      title: "Translate to Slang by Gus",
       contexts: ["selection"],
     });
   });
@@ -9,6 +9,7 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "translate") {
       const selectedText = info.selectionText;
+      console.log("Hola Mundo Gus");
       // Aquí necesitas implementar la lógica para llamar a Google Palm 2 y traducir el texto.
       // Después de recibir la traducción, puedes usar Google Text to Speech para leer el texto traducido.
     }
