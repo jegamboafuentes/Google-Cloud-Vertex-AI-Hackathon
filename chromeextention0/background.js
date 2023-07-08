1a8329e9f96a31e79254ff3410b92460f25685fb
GustablEnano <EliasOrdazS.1995@gmail.com>
2023-07-07 22:51:37 -0600

    Creacion de estructura principal

---
+++ chromeextention0/background.js
@@ -0,0 +1,16 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "translate",
+      title: "Translate to Slang",
+      contexts: ["selection"],
+    });
+  });
+  
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "translate") {
+      const selectedText = info.selectionText;
+      // Aquí necesitas implementar la lógica para llamar a Google Palm 2 y traducir el texto.
+      // Después de recibir la traducción, puedes usar Google Text to Speech para leer el texto traducido.
+    }
+  });
+  
